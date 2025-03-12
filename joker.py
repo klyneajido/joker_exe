1867a0940427b7a5f995d807008cd37cb00231a1
Andriq Klyne Ajido <ajido.andriqklyne@gmail.com>
2025-03-12 21:35:34 +0800

    feat: added dialogue for starting game

---
+++ joker.py
@@ -0,0 +1,51 @@
+import time
+import os
+import sys
+
+def persist():
+    while True:
+        if os.name == "nt":
+            os.system("start /min python "+sys.argv[0])
+        else:
+            os.system("nohup python3 "+sys.argv[0]+" &")
+        time.sleep(5)
+        
+def start_game():
+    print("It is important you pay attention to this message right now")
+    time.sleep(2)
+    print("Take a minute to relax, breathe, and really dig into it.")
+    time.sleep(2)
+    print("'Cause we're about to discuss a deal between you and me, and I don't play games.")
+    time.sleep(4)
+    print("You don't know me however I know you very very very well, and you must be wondering how, correct?")
+    time.sleep(3)
+    print("Well, you've been a bit careless lately, scrolling through those videos and clicking on links, stumbling upon some not-so-safe sites.")
+    time.sleep(4)
+    print("I actually placed a Malware on a porn website & you visited it to watch (you know what I mean)")
+    time.sleep(3)
+    print("And when you got busy enjoying those videos, your system started working as a RDP (Remote Protocol) which provided me complete control over your device")
+    time.sleep(4)
+    print("I can peep at everything on your screen, switch on your camera and mic, and you won't even notice")
+    time.sleep(3)
+    print("Been keeping tabs on your PATHETIC life for a while now. It is simply you hard luck that I saw your blunder.")
+    time.sleep(4)
+    print("Extracted quite a bit of juicy info from your system and I've seen it all.")
+    time.sleep(2)
+    print("Yeah, Yeah, I've got footage of you doing embarrassing things in your room.")
+    time.sleep(2)
+    print("I then developed videos and screenshots where on one side of the screen, there's whatever garbage you had been watching.")
+    time.sleep(3)
+    print("Simply a single click, I can send this garbage to all of your contacts. HAHAHAHAHA")
+    time.sleep(2)
+    print("Your confusion is clear, but don't expect sympathy. I will give you two alternatives.")
+    time.sleep(3)
+    print("First Option is to ignore my message")
+    time.sleep(2)
+    print("You should know what will happen if you choose this option. I will send your video to all of your contacts. The video is straight fire, and I can't even fathom the humiliation you'll endure when your colleagues, friends, and fam check it out. But hey, that's life, ain't it? Don't be playing the victim here.")        
+    time.sleep(6)
+    print("Option 2 is to play a game. A game that is tailored only for a sick and disgusting human being.")
+    time.sleep(2)
+    print("Let's see what happens if you choose this option. Your filthy secret remains private. I will wipe everything clean once you")   
+    time.sleep(2)
+    print(" The email and wallet are custom-made for you, untraceable. If I notice that you've shared or discussed this email with someone else, the video will instantly start getting sent to your contacts. And don't even think about turning off your device or resetting it to factory settings. It's pointless. I don't make mistakes, (name here)")
+start_game()
